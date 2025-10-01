f615294cdac7e2377657268e28107c4d58228913
James Beck <james@jamesbeck.co.uk>
2025-10-01 13:53:52 +0100

    fix

---
+++ app/(site)/our-school/page.tsx
@@ -0,0 +1,23 @@
+import Banner from "@/components/banner";
+import Container from "@/components/container";
+import payload from "@/payload";
+import H2 from "@/components/ui/h2";
+import StaffCard from "@/components/staffCard";
+import { StaffGroup } from "@/payload-types";
+import Breadcrumbs from "@/components/breadcrumbs";
+import H1 from "@/components/ui/h1";
+
+export default async function StaffPage() {
+  return (
+    <div>
+      <Banner url={"/hero/1.png"} focalX={50} focalY={30} />
+      <Breadcrumbs crumbs={[{ label: "Our School", url: "/our-school" }]} />
+
+      <Container>
+        <div className="flex flex-col space-y-8">
+          <H1>Our School</H1>
+        </div>
+      </Container>
+    </div>
+  );
+}
